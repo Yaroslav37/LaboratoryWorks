2acd26f1ae2089bbc59ed495506c1988dcf9e848
Yaroslav37 <minenkovyaroslav@mail.ru>
2022-03-03 12:27:32 +0300

    Добавьте файлы проекта.

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a, b, c;
+            a = 10; b = 2;
+            c = a / b;
+            Assert.AreEqual(5, c);
+        }
+    }
+
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a, b, c;
+            a = 1; b = 228;
+            c = a / b;
+            Assert.AreEqual(0, c);
+        }
+    }
+}
---
+++ _153502_Minenkov/Program.cs
@@ -0,0 +1,10 @@
+﻿int a, b, c;
+
+Console.Write("Введите 1-ое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+c = a / b;
+Console.Write($"Частное от деления чисел : {c}");
